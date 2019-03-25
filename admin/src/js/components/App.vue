54e89712b7bc81f935b015356bd249f8603bd3ea
Pol <polgrammer@gmail.com>
2019-03-25 04:55:09 +0100

    improve frontend

--- admin/src/js/components/App.vue
+++ admin/src/js/components/App.vue
@@ -1,25 +1,47 @@
 <template>
-	<div>
-		<checkblock 
+	<div class="container">
+		<div class="header">
+			<h1>WordPress Security Best Practices</h1>
+		</div>
+		<div class="separator"></div>
+		<div class="section-progress">
+			<!-- attributes: https://vue-multiple.github.io/progress/#attributes -->
+			<VmProgress 
+				 :percentage="percentage" 
+				 type="circle" 
+				 stroke-color="#393939" 
+				 stroke-linecap="square"
+				 :width="200"
+				 stroke-width="25"
+			 >
+	 		</VmProgress>
+		</div>
+		<div class="separator"></div>
+		<div class="section-checks">
+			<checkblock 
 			v-for="checkinfo in info" 
 			:checkinfo="checkinfo" 
 			:key="checkinfo.id"
 			@fix="fix"
 		>
 		</checkblock>
+		</div>
 	</div>
 </template>
 <script>
 import checkblock from './checkblock.vue';
+import VmProgress from 'vue-multiple-progress'
 import axios from 'axios';
 export default{
 	data() {
 		return {
-			info: []
+			info: [],
+			percentage: 69,
 		}
 	},
 	components:{
-		checkblock
+		checkblock,
+		VmProgress
 	},
 	mounted() {
 		this.getInfo()
