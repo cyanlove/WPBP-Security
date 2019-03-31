67fd3a19efc63d31cc083f90f964ea3e37a125ef
Pol <polgrammer@gmail.com>
2019-03-31 13:47:35 +0200

    front improve v2

--- admin/src/js/components/checkblock.vue
+++ admin/src/js/components/checkblock.vue
@@ -1,29 +1,37 @@
 <template>
-	<div id="checkblock">
-		<div 
-			class="accordion"
-			v-bind:class="checkinfo.status"  	
-			@click="togAccordeon"
-		>	
-			{{ checkinfo.short_desc }} 	
-
-			<div id="inputs_desc">
-				<button 
-					id="fix" 
-					v-show="checkinfo.action" 
-					@click="$emit('fix',checkinfo.action)"
-				>
-				 Fix
+	<nav class="drop-down-menu">
+	    <input 
+		  	class ="activate" 
+		  	type  ="checkbox" 
+		  	:id   ="'accordion-' + key" 
+		  	:name ="'accordion-' + key"
+		  	@click="arrowSwap"
+	  	>
+	    <label 
+			:for  ="'accordion-' + key" 
+			class ="menu-title"
+	    >
+		  	<div class="nav-left">
+		  		<div v-bind:class="checkinfo.status"></div>
+		  		<strong>{{ checkinfo.short_desc }}</strong>
+		  	</div>
+		  	<div class="nav-right">
+		  		<button 
+						id="fix" 
+						class="button" 
+						v-show="checkinfo.action" 
+						@click="$emit('fix',checkinfo.action)"
+				>Fix
 				</button>
-				<div id="show-icon" >
-					{{ !isOpen ? '+' : '-' }}
-				</div>
-			</div>
-		</div>
-		<div class="panel">
-			<p><strong>Message:</strong> {{ checkinfo.message }}</p>
-		</div>
-	</div>
+				<span class="dashicons dashicons-arrow-down" id="arrow"></span>
+		  	</div>	
+	    </label>
+	    <div class="drop-down">
+	  		<div class="drop-down-content">
+	  			<span><strong>Message: </strong>{{ checkinfo.message }}</span>
+	  		</div>
+	    </div>
+    </nav>
 </template>
 <script>
 export default{
@@ -34,27 +42,106 @@ export default{
 		}
 	},
 	data(){
-		return{
-			isOpen: false,
+		return {
+			key: this.$vnode.key,
+			currdeg: 0,
 		}
 	},
-	methods:{
-		togAccordeon(event){	
-			var panel = event.target.nextElementSibling;
-			if ( event.target.id == 'fix' ){
-				return
-			}
-			if ( event.target.id == 'show-icon' ){
-				var panel = event.target.parentNode.parentNode.nextElementSibling;
-			}
-		    if ( panel.style.display === "block" ) {
-		      panel.style.display = "none";
-		      this.isOpen = !this.isOpen;
-		    } else {
-		      panel.style.display = "block";
-		      this.isOpen = !this.isOpen;
-		    }
+	methods: {
+		arrowSwap(event){
+			this.currdeg = this.currdeg + 180
+			const arrow = event.target.nextElementSibling.childNodes[2].childNodes[2]
+
+			arrow.style.transform = `rotateX(${this.currdeg}deg)`			
 		}
 	}
 }
-</script> 
+</script>
+<style scoped>
+/*dropdown container*/
+.drop-down-menu {
+	display: block;	
+	box-shadow: 0 1px 1px rgba(0,0,0,.04);
+	margin-bottom: 10px;
+	box-sizing: border-box;
+}
+/*input*/
+.activate {
+	display: none;
+	position: absolute;
+	cursor: pointer;
+	width: 100%;
+	height: 40px;
+	margin: 0 0 0 -15px;
+	opacity: 0;
+}
+/*label*/
+.menu-title {
+	cursor: pointer;	
+	display:block;
+	padding: 10px;
+	height: 50px;
+	background: #FAFAFA;
+	border: 1px solid #e5e5e5;
+	transition: all 0.2s;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	box-sizing: border-box;
+}
+.menu-title:hover {
+	border: 1px solid rgb(68, 68, 68);
+}
+.nav-right, .nav-left{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.fail {
+	width: 12px;
+	height: 12px;
+	margin-right: 10px;
+	border-radius: 50%;
+    background-color: #FF4545;
+    box-shadow: #EBE9E9 0 -1px 1px 1px, inset #A62C2C 0 -1px 4px, #898989 0 3px 12px;
+}
+.pass{
+	width: 12px;
+	height: 12px;
+	margin-right: 10px;
+	border-radius: 50%;
+    background-color: #33E248;
+    box-shadow: #EBE9E9 0 -1px 5px 1px, inset #56A058 0 -1px 4px, #898989 0 3px 12px;
+}
+#fix{
+	margin-right: 10px;
+	width: 80px;
+	color: #72777c;
+}
+/*dropdown content*/
+.drop-down {
+	max-height: 0;
+	overflow: hidden;
+}
+.drop-down-content{
+	min-height: 70px;
+	padding: 10px 20px 10px 20px;
+	display: flex;
+	justify-content: left;
+	align-items: center;
+	background: #fff;
+	border: 1px solid #e5e5e5;
+	border-top: 0px;
+}
+/*checked:*/
+.activate:checked ~ .drop-down {
+	max-height: 200px;
+}
+/*transitions*/
+.drop-down, .menu span, .drop-down-menu, #arrow {
+	-webkit-transition: all 0.4s ease;
+	-moz-transition: all 0.4s ease;
+	-o-transition: all 0.4s ease;
+	transition: all 0.4s ease;
+}
+</style>
